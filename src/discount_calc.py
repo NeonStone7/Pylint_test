c857ee4d177f122819759b17a2c67798c954155f
Oamen_Modupe <84418297+NeonStone7@users.noreply.github.com>
2023-07-17 21:13:18 +0100

    Update discount_calc.py

--- src/discount_calc.py
+++ src/discount_calc.py
@@ -1,7 +1,7 @@
 '''This module calculates discount after payment'''
-def discount_calculator(original_price, discount_percentage):   
+def discount_calculator(original_price, discount_percentage):
     """This function calculates the final price after removing discount.
     User inputs Original_price: price of item
-    discount_percentage: Discount you are offering customer e.g input 35% as 0.35"""   
+    discount_percentage: Discount you are offering customer e.g input 35% as 0.35"""
     final_price = original_price -(discount_percentage*original_price)
     return final_price
